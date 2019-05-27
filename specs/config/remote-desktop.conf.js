ebccf02c031f8ba427d3c9c8a00cb7561c9b4600
nikodamn <nikodem.hynek@wikia-inc.com>
2019-05-27 16:19:57 +0200

    Add remote configs

---
+++ specs/config/remote-desktop.conf.js
@@ -0,0 +1,34 @@
+const merge = require('deepmerge');
+
+const baseConfig = require('./base.conf');
+
+global.wdioEnvironment = 'desktop';
+
+exports.config = merge(
+    baseConfig.config,
+    {
+        suites: {
+            example: ['./**/suites/**/example.desktop.js'],
+        },
+        specs: ['**/suites/**/*.desktop.js'],
+        exclude: ['**/suites/**/*.mobile.js'],
+        hostname: 'selenium-standalone',
+        port: 4444,
+        path: '/wd/hub',
+        specFileRetries: 1,
+        capabilities: [{
+            maxInstances: 4,
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                args: ['--headless', '--disable-gpu', '--no-sandbox', '--ignore-certificate-errors', '--disable-extensions', '--window-size=1366,768', '--whitelisted-ips'],
+            },
+        }],
+        logLevel: 'error',
+        baseUrl: 'selenium-standalone',
+
+        onPrepare() {
+            setTimeout(() => { console.log('Wait before all workers got launched'); }, 5000);
+        },
+    }
+);
