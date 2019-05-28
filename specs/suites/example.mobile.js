c6b90fff663bf8347e663d0540db7375f827ea96
nikodamn <nikodem.hynek@wikia-inc.com>
2019-05-28 15:29:30 +0200

    Revert breaking change for mobile test

--- specs/suites/example.mobile.js
+++ specs/suites/example.mobile.js
@@ -3,6 +3,6 @@ const { expect } = require('chai');
 describe('[Mobile] First suite', () => {
     it('main page test', () => {
         browser.url('http://nikodamn.github.io')
-        expect($('.not-existing-class').waitForDisplayed(), 'Logo should be visible').to.be.true;
+        expect($('.site-title').waitForDisplayed(), 'Logo should be visible').to.be.true;
     })
 });
